85a9db48628491a21d77f0f53cb6c09d05ddc025
Lina Charane <charaneluna@gmail.com>
2026-09-02 02:20:29 +0200

    Learned constructors, mrthod override ...

---
+++ OOP_INT/src/User.java
@@ -0,0 +1,40 @@
+
+public class User {
+    private String _name;
+    private String membership = "Bronze";
+
+
+    public User(){
+
+    }
+    // Lets make a constructor so that we can use the class just as python User("ELY", "Gold")
+    public User( String name , String membership){
+        set_name(name) ;
+        set_membership(membership); ;
+    }
+    void set_name(String name){ // this is the set method
+        this._name = name ; // can also do _name = name
+                                                             }
+    void set_membership(String membership){ // void means it does not return an output
+        this.membership = membership;
+    }
+    String get_membership(){
+        return membership ;
+    }
+    void set_membership(Membership membership){
+        this.membership = membership.name() ;
+    }
+    public enum Membership{
+        Silver,Gold, Platinum ;
+    }
+    /*void set_membership(Int membership ){
+        this.membership = membership
+        };*/
+    public String toString(){
+        return get_name() + " " + get_membership() ;
+    }
+    String get_name() {
+        System.out.println("Logging to system files!");
+        return _name ;
+    }
+}
--- OOP_INT/src/main.java
+++ OOP_INT/src/main.java
@@ -1,6 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        
+        User u = new User("Caleb", "Gold");
+        User u2 = new User() ;
+        u2.set_name("Eve") ;
+        System.out.println(u);
+
     }
 }
 
